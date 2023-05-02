4f6c3351bfbc1d048bc327185de2fec937374774
hyagotsaky <hyagotsaky@gmail.com>
2023-05-02 14:55:14 -0300

    anotacoes

--- src/main/java/domain/Chamado.java
+++ src/main/java/domain/Chamado.java
@@ -1,22 +1,40 @@
 package domain;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import enums.Prioridade;
 import enums.Status;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
-public class Chamado {
+@Entity
+public class Chamado implements Serializable {
+	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dataAbertura = LocalDate.now();
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dataFechamento;
 	private Prioridade prioridade;
 	private Status status;
 	private String titulo;
 	private String observacoes;
 	
+	@ManyToOne
+	@JoinColumn(name = "tecnico_id")
 	private Tecnico tecnico;
+	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
 	
 	
--- src/main/java/domain/Tecnico.java
+++ src/main/java/domain/Tecnico.java
@@ -3,16 +3,26 @@ package domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import enums.Perfil;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+@Entity
 public class Tecnico extends Pessoa {
 
+	private static final long serialVersionUID = 1L;
+	
+	@OneToMany(mappedBy = "tecnico")
 	private List<Chamado> chamado = new ArrayList<>();
 
 	public Tecnico() {
 		super();
+		addPerfis(Perfil.CLIENTE);
 	}
 
 	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
+		addPerfis(Perfil.CLIENTE);
 	}
 
 	public List<Chamado> getChamado() {

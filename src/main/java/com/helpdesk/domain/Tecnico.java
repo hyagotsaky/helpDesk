62e1a83da59e1b1a0ad5cd31a7bf79bfed5b6301
hyagotsaky <hyagotsaky@gmail.com>
2023-05-04 10:37:33 -0300

    corrige problema de serializacao

--- src/main/java/com/helpdesk/domain/Tecnico.java
+++ src/main/java/com/helpdesk/domain/Tecnico.java
@@ -3,6 +3,7 @@ package com.helpdesk.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.helpdesk.enums.Perfil;
 
 import jakarta.persistence.Entity;
@@ -13,6 +14,7 @@ public class Tecnico extends Pessoa {
 
 	private static final long serialVersionUID = 1L;
 	
+	@JsonIgnore
 	@OneToMany(mappedBy = "tecnico")
 	private List<Chamado> chamado = new ArrayList<>();
 

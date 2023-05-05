26f36c5ac1472f6e1736b327380c50227e3c60e7
hyagotsaky <hyagotsaky@gmail.com>
2023-05-04 23:08:49 -0300

    endpoint findAll para tecnico

--- src/main/java/com/helpdesk/resources/TecnicoResource.java
+++ src/main/java/com/helpdesk/resources/TecnicoResource.java
@@ -1,5 +1,8 @@
 package com.helpdesk.resources;
 
+import java.util.List;import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,6 +26,14 @@ public class TecnicoResource {
 		Tecnico obj = service.findById(id);
 		return ResponseEntity.ok(new TecnicoDto(obj));
 		
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<TecnicoDto>> findAll(){
+		 List<Tecnico> list = service.findAll();
+		 List<TecnicoDto> listDTO = list.stream().map(obj -> new TecnicoDto(obj)).collect(Collectors.toList());
+		 System.out.println(listDTO);
+		 return ResponseEntity.ok().body(listDTO);
 	}
 	
 }
